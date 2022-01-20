fdd9b791e595c44bb886a461160b0fe8ff3c4be1
gillec1 <97477062+gillec1@users.noreply.github.com>
2022-01-19 21:38:22 -0500

    Delete Numpy Essential Training 1 - Foundations of NumPy Certificate.py

--- Numpy Essential Training 1 - Foundations of NumPy Certificate.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# **Numpy Essential Training 1: Foundations of NumPy Certificate**
-
-# ![Screen%20Shot%202022-01-19%20at%209.33.17%20PM.png](attachment:Screen%20Shot%202022-01-19%20at%209.33.17%20PM.png)
